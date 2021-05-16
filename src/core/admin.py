5f42652d07388681d026f0350e98b076fe2d6d7a
Vishal Tanwar <vishal.tanwar@outlook.com>
2021-05-16 14:32:17 +0530

    only XLSX and CSV allowed

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
+from import_export.formats.base_formats import CSV, XLSX
 
 from core.models import Student
 
@@ -8,3 +9,4 @@ from core.models import Student
 class StudentAdmin(ImportExportModelAdmin):
 
     list_display = ('id', 'first_name', 'last_name', 'registration_number', 'gender', 'date_of_admission')
+    formats = [XLSX, CSV]
